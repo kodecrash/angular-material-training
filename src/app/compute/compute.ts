7140a5135db36824ff9a8c994f29c35ea4ba2e4f
Kodecrash - Web Developer Solution <prathameshk73@outlook.com>
2020-07-21 13:11:03 +0530

    Update compute.ts

--- src/app/compute/compute.ts
+++ src/app/compute/compute.ts
@@ -7,3 +7,34 @@ export function Compute(num: number) {
 
     return num + 1;
 }
+
+import {Component, Output, EventEmitter, Input, OnInit} from '@angular/core';
+import { TableService } from './table.service';
+
+@Component({
+    selector: 'custom-component',
+    template: `
+    <input [(ngModel)]="rowData.quantity" [value]="rowData.quantity" (ngModelChange)="onModelChange()"/>`
+})
+export class CustomComponent implements OnInit {
+
+   /// rowData: any;
+ 
+  @Input() rowData: any;
+
+  constructor(private tableService: TableService) {
+
+  }
+
+   @Output() save: EventEmitter<any> = new EventEmitter();
+
+   ngOnInit() {
+     console.log(this.rowData);
+   }
+
+    onModelChange(table) {
+        this.rowData.total = this.rowData.amount * this.rowData.price;
+        this.tableService.rowUpdateSubject.next(this.rowData);
+        this.save.emit(this.rowData);
+    }
+}
